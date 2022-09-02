bc77c343a80f0a132e26f3667eca3d1a7c92893c
anderson-felix <andersonfelixmsouza@gmail.com>
2022-09-02 13:03:23 -0300

    fix: update display created_at value for logs

--- src/modules/log/utils/formatLogEntity.ts
+++ src/modules/log/utils/formatLogEntity.ts
@@ -1,5 +1,6 @@
 import Log from '../infra/typeorm/entities/Log';
 
+const TIMEZONE = 1000 * 60 * 60 * 3;
 export interface IFormattedLog extends Omit<Log, 'created_at'> {
   created_at: string;
 }
@@ -8,7 +9,7 @@ type FuncType = (log: Log) => IFormattedLog;
 
 export const formatLogEntity: FuncType = log => ({
   ...log,
-  created_at: `${new Date(log.created_at).toDateString()} - ${new Date(
-    log.created_at,
+  created_at: `${log.created_at.toDateString()} - ${new Date(
+    log.created_at.getTime() - TIMEZONE,
   ).toLocaleTimeString()}`,
 });
